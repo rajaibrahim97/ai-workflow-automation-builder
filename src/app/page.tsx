3a82e2c3a138ba08bfbf9660069f8b779c1a76b3
rajaibrahim97 <ibi.ri19@gmail.com>
2026-02-12 19:00:24 +0500

    04 -feat: implement authentication with better-auth and create login/signup forms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,14 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
-import { Client } from "./client";
+import { requireAuth } from "@/lib/auth-utils"
+import { caller } from "@/trpc/server"
 
 const Page = async () => {
-  
-  const queryClient = getQueryClient();
+  await requireAuth()
 
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
-
+  const data = await caller.getUsers();
   return (
-    <div className='min-h-screen min-w-screen flex items-center justify-center'>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<p>loading...</p>}>
-          <Client/>
-        </Suspense>
-      </HydrationBoundary>
+    <div className='min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6'>
+      protected server component
+      {JSON.stringify(data,null,2)}
     </div>
   )
 }
