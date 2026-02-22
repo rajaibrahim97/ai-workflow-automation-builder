1a03c3a8013930ccb3da546f9f9fe444974ea8f5
rajaibrahim97 <ibi.ri19@gmail.com>
2026-02-22 16:39:46 +0500

    15: editor setup

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -1,24 +1,79 @@
 "use client";
-
+import { useState, useCallback, useMemo } from 'react';
+import {
+  ReactFlow,
+  applyNodeChanges,
+  applyEdgeChanges,
+  addEdge,
+  type Node,
+  type Edge,
+  type NodeChange,
+  type EdgeChange,
+  type Connection,
+  Background,
+  Controls,
+  MiniMap,
+  Panel,
+} from '@xyflow/react';
 import { ErrorView, LoadingView } from "@/components/entity-components";
-import { useSuspenseWorkflow  } from "@/features/workflows/hooks/use-workflows";
+import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows";
+import { nodeComponents } from '@/config/node-components';
+import { AddNodeButton } from './add-node-button';
 
 
 export const EditorLoading = () => {
-    return <LoadingView message="Loading editor.." />
+  return <LoadingView message="Loading editor.." />
 };
 
 export const EditorError = () => {
-    return <ErrorView message="Error loading editor" />
+  return <ErrorView message="Error loading editor" />
 }
 
-export const Editor = ({ workflowId }:{ workflowId:string }) => {
-    const { data: workflow } = useSuspenseWorkflow(workflowId);
+/**
+ * Nodes setting
+ */
 
-    return (
-        <p>
-            {JSON.stringify(workflow, null, 2)}
-        </p>
-    )
+
+export const Editor = ({ workflowId }: { workflowId: string }) => {
+  const { data: workflow } = useSuspenseWorkflow(workflowId);
+  const [nodes, setNodes] = useState<Node[]>(workflow.nodes);
+  const [edges, setEdges] = useState<Edge[]>(workflow.edges);
+
+  const onNodesChange = useCallback(
+    (changes: NodeChange[]) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    [],
+  );
+  const onEdgesChange = useCallback(
+    (changes: EdgeChange[]) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    [],
+  );
+  const onConnect = useCallback(
+    (params: Connection) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    [],
+  );
+
+  return (
+    <div className='size-full'>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        nodeTypes={nodeComponents}
+        fitView
+        proOptions={{
+          hideAttribution: true
+        }}
+      >
+        <Background />
+        <Controls />
+        <MiniMap />
+        <Panel position='top-right'>
+          <AddNodeButton />
+        </Panel>
+      </ReactFlow>
+    </div>
+  )
 };
 
--- src/features/workflows/server/routers.ts
+++ src/features/workflows/server/routers.ts
@@ -1,94 +1,129 @@
-import {generateSlug} from "random-word-slugs";
+import { generateSlug } from "random-word-slugs";
 import prisma from "@/lib/db";
 import { createTRPCRouter, premiumProcedure, protectedProcedure } from "@/trpc/init";
 import z from "zod";
 import { PAGINATION } from "@/config/constants";
+import type { Node, Edge } from "@xyflow/react";
+import {NodeType} from "@/generated/prisma"
 
 export const workflowsRouter = createTRPCRouter({
-    create: protectedProcedure.mutation(({ctx})=>{
+    create: premiumProcedure.mutation(({ ctx }) => {
         return prisma.workflow.create({
-            data:{
-                name:generateSlug(3),
-                userId: ctx.auth.user.id
-            }
+            data: {
+                name: generateSlug(3),
+                userId: ctx.auth.user.id,
+                nodes: {
+                    create: {
+                        type: NodeType.INITIAL,
+                        position: { x: 0, y: 0 },
+                        name: NodeType.INITIAL,
+                    },
+                },
+            },
         })
     }),
     remove: protectedProcedure
-    .input(z.object({id:z.string()}))
-    .mutation(({ctx,input})=>{
-        return prisma.workflow.delete({
-            where: {
-                id:input.id,
-                userId: ctx.auth.user.id
-            }
-        })
-    }),
+        .input(z.object({ id: z.string() }))
+        .mutation(({ ctx, input }) => {
+            return prisma.workflow.delete({
+                where: {
+                    id: input.id,
+                    userId: ctx.auth.user.id
+                }
+            })
+        }),
     updateName: protectedProcedure
-    .input(z.object({id:z.string(),name:z.string().min(1)}))
-    .mutation(({ctx,input}) => {
-        return prisma.workflow.update({
-            where: {id: input.id, userId:ctx.auth.user.id},
-            data: {name:input.name},
-        })
-    }),
+        .input(z.object({ id: z.string(), name: z.string().min(1) }))
+        .mutation(({ ctx, input }) => {
+            return prisma.workflow.update({
+                where: { id: input.id, userId: ctx.auth.user.id },
+                data: { name: input.name },
+            })
+        }),
 
     getOne: protectedProcedure
-    .input(z.object({id:z.string()}))
-    .query(({ctx,input}) => {
-        return prisma.workflow.findUniqueOrThrow({
-            where: {id:input.id, userId:ctx.auth.user.id}
-        });
-    }),
+        .input(z.object({ id: z.string() }))
+        .query( async ({ ctx, input }) => {
+            const workflow = await prisma.workflow.findUniqueOrThrow({
+                where: { id: input.id, userId: ctx.auth.user.id },
+                include: { nodes:true, connections: true}
+            });
+
+            // Transform server nodes to react-flow compatible nodes
+            const nodes:Node[] = workflow.nodes.map((node) => ({
+                id:node.id,
+                type: node.type,
+                position: node.position as { x: number, y: number},
+                data: (node.data as Record<string, unknown>) || {},
+            }));
+
+            // Transform server connection to react-flow compatible edges
+            const edges: Edge[] = workflow.connections.map((connection)=>({
+                id:connection.id,
+                source: connection.fromNodeId,
+                target: connection.toNodeId,
+                sourceHandle: connection.fromOutput,
+                targetHandle: connection.toInput,
+            }));
+
+            return {
+                id: workflow.id,
+                name: workflow.name,
+                nodes,
+                edges,
+            };
+        }),
     getMany: protectedProcedure
-    .input(z.object({
-        page: z.number().default(PAGINATION.DEFAULT_PAGE),
-        pageSize: z
-        .number()
-        .min(PAGINATION.MIN_PAGE_SIZE)
-        .max(PAGINATION.MAX_PAGE_SIZE)
-        .default(PAGINATION.DEFAULT_PAGE_SIZE),
-        search: z.string().default(""),
-    }))
-    .query(async ({ctx,input}) => {
-        const {page, pageSize, search} = input;
+        .input(z.object({
+            page: z.number().default(PAGINATION.DEFAULT_PAGE),
+            pageSize: z
+                .number()
+                .min(PAGINATION.MIN_PAGE_SIZE)
+                .max(PAGINATION.MAX_PAGE_SIZE)
+                .default(PAGINATION.DEFAULT_PAGE_SIZE),
+            search: z.string().default(""),
+        }))
+        .query(async ({ ctx, input }) => {
+            const { page, pageSize, search } = input;
 
-        const [items, totalCount] = await Promise.all([
-            prisma.workflow.findMany({
-                skip: (page-1) * pageSize,
-                take:pageSize,
-                where: {
-                    userId: ctx.auth.user.id,
-                    name: {
-                        contains: search,
-                        mode:"insensitive",
+            const [items, totalCount] = await Promise.all([
+                prisma.workflow.findMany({
+                    skip: (page - 1) * pageSize,
+                    take: pageSize,
+                    where: {
+                        userId: ctx.auth.user.id,
+                        name: {
+                            contains: search,
+                            mode: "insensitive",
+                        },
+                        
                     },
-                },
-                orderBy:{
-                    updatedAt:"desc"
-                }
-            }),
-            prisma.workflow.count({
-                where: {
-                    userId: ctx.auth.user.id,
-                    name: {
-                        contains: search,
-                        mode:"insensitive",
+                    orderBy: {
+                        updatedAt: "desc"
+                    }
+                }),
+                prisma.workflow.count({
+                    where: {
+                        userId: ctx.auth.user.id,
+                        name: {
+                            contains: search,
+                            mode: "insensitive",
+                        },
                     },
-                },
-            })
-        ])
-        const totalPages = Math.ceil(totalCount / pageSize);
-        const hasNextPage = page < totalPages;
-        const hasPreviousPage = page > 1;
+                })
+            ])
+            const totalPages = Math.ceil(totalCount / pageSize);
+            const hasNextPage = page < totalPages;
+            const hasPreviousPage = page > 1;
 
-        return {
-            items,
-            page,
-            pageSize,
-            totalCount,
-            totalPages,
-            hasNextPage,
-            hasPreviousPage,
-        };
-    }),
+            return {
+                items,
+                page,
+                pageSize,
+                totalCount,
+                totalPages,
+                hasNextPage,
+                hasPreviousPage,
+            };
+        }),
 })
