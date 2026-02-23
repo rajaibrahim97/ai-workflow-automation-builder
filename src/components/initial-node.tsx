5da102037d57b8015de43cc0382c0716808fcbc7
rajaibrahim97 <ibi.ri19@gmail.com>
2026-02-23 14:21:18 +0500

    16: node selector

--- src/components/initial-node.tsx
+++ src/components/initial-node.tsx
@@ -4,19 +4,23 @@ import { PlusIcon } from "lucide-react";
 import { memo, useState } from "react";
 import { PlaceholderNode } from "./react-flow/placeholder-node";
 import { WorkflowNode } from "./workflow-node";
+import { NodeSelector } from "./node-selecter";
 
 export const InitialNode = memo((props: NodeProps) => {
+    const [selecterOpen, setSelectorOpen] = useState(false);
     return (
+        <NodeSelector open={selecterOpen} onOpenChange={setSelectorOpen}>
         <WorkflowNode showToolBar={false}>
             <PlaceholderNode
                 {...props}
-                onClick={()=>{}}
+                onClick={()=>setSelectorOpen(true)}
             >
                 <div className="cursor-pointer flex items-center justify-center">
                     <PlusIcon className="size-4" />
                 </div>
             </PlaceholderNode>
         </WorkflowNode>
+        </NodeSelector>
     )
 });
 
