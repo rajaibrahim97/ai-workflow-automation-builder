89531fff8576a11a2c65b59225c261994320f569
rajaibrahim97 <ibi.ri19@gmail.com>
2026-02-24 15:56:34 +0500

    17: editor state

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -19,6 +19,8 @@ import { ErrorView, LoadingView } from "@/components/entity-components";
 import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows";
 import { nodeComponents } from '@/config/node-components';
 import { AddNodeButton } from './add-node-button';
+import { useSetAtom } from 'jotai';
+import { editorAtom } from '../store/atoms';
 
 
 export const EditorLoading = () => {
@@ -35,7 +37,9 @@ export const EditorError = () => {
 
 
 export const Editor = ({ workflowId }: { workflowId: string }) => {
+
   const { data: workflow } = useSuspenseWorkflow(workflowId);
+  const setEditor = useSetAtom(editorAtom)
   const [nodes, setNodes] = useState<Node[]>(workflow.nodes);
   const [edges, setEdges] = useState<Edge[]>(workflow.edges);
 
@@ -61,7 +65,13 @@ export const Editor = ({ workflowId }: { workflowId: string }) => {
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
         nodeTypes={nodeComponents}
+        onInit={setEditor}
         fitView
+        snapGrid={[10,10]}
+        snapToGrid
+        panOnScroll
+        panOnDrag={false}
+        selectionOnDrag
         proOptions={{
           hideAttribution: true
         }}
