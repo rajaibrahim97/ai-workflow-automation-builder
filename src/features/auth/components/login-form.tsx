74af2d21163df0d5bcaae05157afe4d9fb800af1
rajaibrahim97 <ibi.ri19@gmail.com>
2026-05-11 18:47:54 +0500

    28: github/google auth

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -44,6 +44,32 @@ export function LoginForm(){
         },
     });
 
+    const signInGithub = async () => {
+        await authClient.signIn.social({
+            provider:"github"
+        },{
+            onSuccess: () => {
+                router.push('/');
+            },
+            onError: () => {
+                toast.error("Something went wrong")
+            },
+        })
+    };
+
+    const signInGoogle = async () => {
+        await authClient.signIn.social({
+            provider:"google"
+        },{
+            onSuccess: () => {
+                router.push('/');
+            },
+            onError: () => {
+                toast.error("Something went wrong")
+            },
+        })
+    };
+
     const onSubmit = async (values:LoginFormValues) => {
         await authClient.signIn.email({
             email:values.email,
@@ -79,6 +105,7 @@ export function LoginForm(){
                             <div className="grid gap-6">
                                 <div className="flex flex-col gap-4">
                                     <Button 
+                                    onClick={signInGithub}
                                     variant="outline" 
                                     className="w-full"
                                      type="button" 
@@ -88,6 +115,7 @@ export function LoginForm(){
                                         Continue with GitHub
                                     </Button>
                                     <Button 
+                                    onClick={signInGoogle}
                                     variant="outline" 
                                     className="w-full"
                                      type="button" 
